898619e7cde0d0aa1150b73ad9ffe36018e9fc7e
kerasing <kate@rasing.org>
2017-05-20 15:29:14 -0500

    including the rest of the scoring methods
    this now has all ways to score in yahtzee, minus the bonus

--- Yahtzee/Yahtzee/Yahtzee.py
+++ Yahtzee/Yahtzee/Yahtzee.py
@@ -1,28 +1,98 @@
+from enum import Enum
+class YahtzeeScore(Enum):
+    Ones = 1
+    Twos = 2
+    Threes = 3
+    Fours = 4
+    Fives = 5
+    Sixes = 6
+    Pair = 7
+    TwoPair = 8
+    ThreeOfAKind = 9
+    FourOfAKind = 10
+    SmallStraight = 11
+    LargeStraight = 12
+    FullHouse = 13
+    Yahtzee = 14
+    Chance = 15
 
-def scoreNumber(dice, matching_die_face):
-    return (countNumberOfDiceOfAParticularNumber(dice,matching_die_face)*matching_die_face)
-
-def scorePair(dice):
-    listOfPairs = createListOfPairs(dice)
-    if len(listOfPairs) > 0:
-        return 2*listOfPairs.pop()
+def score(dice, method):    
+    if method == YahtzeeScore.Ones:
+        return scoreNumber(dice,1)    
+    if method == YahtzeeScore.Twos:
+        return scoreNumber(dice,2)    
+    if method == YahtzeeScore.Threes:
+        return scoreNumber(dice,3)    
+    if method == YahtzeeScore.Fours:
+        return scoreNumber(dice,4)    
+    if method == YahtzeeScore.Fives:
+        return scoreNumber(dice,5)    
+    if method == YahtzeeScore.Sixes:
+        return scoreNumber(dice,6)
+    if method == YahtzeeScore.Pair:
+        return scorePair(dice)
+    if method == YahtzeeScore.TwoPair:
+        return scoreTwoPair(dice)
+    if method == YahtzeeScore.ThreeOfAKind:
+        return scoreThreeOfAKind(dice)
+    if method == YahtzeeScore.FourOfAKind:
+        return scoreFourOfAKind(dice)
+    if method == YahtzeeScore.Yahtzee:
+        return scoreFiveOfAKind(dice)
+    if method == YahtzeeScore.SmallStraight:
+        return scoreSmallStraight(dice)
+    if method == YahtzeeScore.LargeStraight:
+        return scoreLargeStraight(dice)
+    if method == YahtzeeScore.FullHouse:
+        return scoreFullHouse(dice)
+    if method == YahtzeeScore.Chance:
+        return scoreChance(dice)
     return 0
 
+def scoreNumber(dice, matching_die_face):
+    return (dice.count(matching_die_face)*matching_die_face)
+
+
+def scoreNMatches(dice, N):
+    listOfNMatches = createListOfDiceThatHaveAtLeastNMatches(dice,N)
+    if len(listOfNMatches) > 0:
+        return N*listOfNMatches.pop()
+    return 0
+
+def scorePair(dice):
+    return scoreNMatches(dice,2)
+
+def scoreThreeOfAKind(dice):
+    return scoreNMatches(dice,3)
+
+def scoreFourOfAKind(dice):
+    return scoreNMatches(dice,4)
+
+def scoreFiveOfAKind(dice):
+    return scoreNMatches(dice,5)
+
+
 def scoreTwoPair(dice):
-    listOfPairs = createListOfPairs(dice)
+    listOfPairs = createListOfDiceThatHaveAtLeastNMatches(dice,2)
     if len(listOfPairs) == 2:
         return 2*sum(listOfPairs)
     return 0
 
-def scoreSmallStraight(dice):
-    diceCount = []
-    for i in list(range(6)):
-        diceCount.append(countNumberOfDiceOfAParticularNumber(dice, i+1))
 
-    all_straights = ((diceCount[2] > 0) & (diceCount[3] >0))
-    straight1 = ((diceCount[0] > 0) & (diceCount[1] > 0))
-    straight2 = ((diceCount[1] > 0) & (diceCount[4] > 0))
-    straight3 = ((diceCount[4] > 0) & (diceCount[5] > 0))
+def scoreFullHouse(dice):
+    diceCounts = findMatches(dice)
+    if ((diceCounts.count(2) > 0) & (diceCounts.count(3) > 0)):
+        return sum(dice)
+    return 0
+
+
+def scoreSmallStraight(dice):
+    diceCount = findMatches(dice)
+
+    all_straights = ((diceCount[2] > 0) & (diceCount[3] > 0))   #all straights have 3 & 4
+    straight1     = ((diceCount[0] > 0) & (diceCount[1] > 0))   #  combo1: 1,2,3,4
+    straight2     = ((diceCount[1] > 0) & (diceCount[4] > 0))   #  combo2: 2,3,4,5
+    straight3     = ((diceCount[4] > 0) & (diceCount[5] > 0))   #  combo3: 3,4,5,6
 
     if all_straights & (straight1 | straight2 | straight3):
         return 30
@@ -40,14 +110,9 @@ def scoreLargeStraight(dice):
 def scoreChance(dice):
     return sum(dice)
 
-def countNumberOfDiceOfAParticularNumber(dice, number):
-    return  len([d for d in dice if d==number])
+def createListOfDiceThatHaveAtLeastNMatches(dice,numberOfMatches):
+    listOfDiceCounts = findMatches(dice)
+    return [i+1 for i in list(range(6)) if listOfDiceCounts[i]>=numberOfMatches]
 
-def createListOfPairs(dice):
-    listOfPairs = []
-    for i in list(range(6)):
-        checking_die = i+1
-        count = countNumberOfDiceOfAParticularNumber(dice, checking_die)
-        if count > 1:
-            listOfPairs.append(checking_die)
-    return listOfPairs
+def findMatches(dice): 
+    return [dice.count(i+1) for i in list(range(6))]
--- Yahtzee/Yahtzee/Yahtzee_tests.py
+++ Yahtzee/Yahtzee/Yahtzee_tests.py
@@ -1,91 +1,128 @@
 import unittest
 import Yahtzee
 
-class Test_Yahtzee_tests(unittest.TestCase):
+class Test_YahtzeeScoring(unittest.TestCase):
 #tests for rule 1: individual dice faces
     def test_Ones(self):
-        ones = roll(1,1,1,1,1)
-        self.assertEqual(Yahtzee.scoreNumber(ones,1), 5)
-        self.assertEqual(Yahtzee.scoreNumber(ones,2), 0)
-        self.assertEqual(Yahtzee.scoreNumber(ones,3), 0)
-        self.assertEqual(Yahtzee.scoreNumber(ones,4), 0)
-        self.assertEqual(Yahtzee.scoreNumber(ones,5), 0)
-        self.assertEqual(Yahtzee.scoreNumber(ones,6), 0)
+        dice = roll(1,1,1,1,1)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Ones),   5)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Twos),   0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Threes), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fours),  0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fives),  0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Sixes),  0)
         
-    def test_dojoExample(self):
+    def test_Roll2(self):
         dice = roll(1,1,2,4,4)
-        self.assertEqual(Yahtzee.scoreNumber(dice,1), 2)
-        self.assertEqual(Yahtzee.scoreNumber(dice,2), 2)
-        self.assertEqual(Yahtzee.scoreNumber(dice,3), 0)
-        self.assertEqual(Yahtzee.scoreNumber(dice,4), 8)
-        self.assertEqual(Yahtzee.scoreNumber(dice,5), 0)
-        self.assertEqual(Yahtzee.scoreNumber(dice,6), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Ones),   2)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Twos),   2)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Threes), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fours),  8)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fives),  0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Sixes),  0)
+        
+    def test_Roll3(self):
+        dice = roll(3,3,5,5,6)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Ones),   0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Twos),   0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Threes), 6)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fours),  0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Fives),  10)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Sixes),  6)
 
 #tests for rule 2: Pair      
     def test_PairWithTwoPair(self):
         dice = roll(3,3,3,4,4)
-        self.assertEqual(Yahtzee.scorePair(dice), 8)    
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Pair),   8)
         
     def test_PairWithNoPairs(self):
         dice = roll(1,3,5,4,6)
-        self.assertEqual(Yahtzee.scorePair(dice), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Pair),   0)
         
 # tests for rule 3: Two Pair
     def test_TwoPairWithNoPairs(self):
         dice = roll(1,3,5,4,6)
-        self.assertEqual(Yahtzee.scoreTwoPair(dice), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.TwoPair),   0)
         
     def test_TwoPairWithOnePair(self):
         dice = roll(1,1,5,4,6)
-        self.assertEqual(Yahtzee.scoreTwoPair(dice), 0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.TwoPair),   0)
         
     def test_TwoPairWithTwoPairs(self):
         dice = roll(1,1,2,3,3)
-        self.assertEqual(Yahtzee.scoreTwoPair(dice), 8)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.TwoPair),   8)
 
 # tests for rule 4: Three of a kind
+    def test_ThreeOAK(self):
+        dice = roll(1,1,1,2,3)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.ThreeOfAKind),   3)
+        
+    def test_ThreeOAKNoMatches(self):
+        dice = roll(1,1,2,2,3)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.ThreeOfAKind),   0)
 
 # tests for rule 5: Four of a kind
+    def test_FourOAK(self):
+        dice = roll(1,1,1,2,1)
+        self.assertEqual(Yahtzee.scoreFourOfAKind(dice), 4)
+        
+    def test_FourOAKNoMatches(self):
+        dice = roll(1,1,2,2,3)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.FourOfAKind),    0)
 
 # tests for rule 6: small straight
     def test_smallStraightOne(self):
         dice = roll(5,2,3,4,1)
-        self.assertEqual(Yahtzee.scoreSmallStraight(dice),30)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.SmallStraight),  30)
         
     def test_smallStraightTwo(self):
         dice = roll(5,2,3,4,6)
-        self.assertEqual(Yahtzee.scoreSmallStraight(dice),30)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.SmallStraight),   30)
 
     def test_smallStraightThree(self):
         dice = roll(5,1,3,4,6)
-        self.assertEqual(Yahtzee.scoreSmallStraight(dice),30)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.SmallStraight),   30)
 
     def test_smallStraightNone(self):
         dice = roll(5,5,1,4,6)
-        self.assertEqual(Yahtzee.scoreSmallStraight(dice),0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.SmallStraight),   0)
 
 # tests for rule 7: large straight
     def test_largeStraightOne(self):
         dice = roll(5,2,3,4,1)
-        self.assertEqual(Yahtzee.scoreLargeStraight(dice),40)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.LargeStraight),   40)
 
     def test_largeStraightTwo(self):
         dice = roll(5,2,3,4,6)
-        self.assertEqual(Yahtzee.scoreLargeStraight(dice),40)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.LargeStraight),   40)
 
     def test_largeStraightNone(self):
         dice = roll(5,5,1,4,6)
-        self.assertEqual(Yahtzee.scoreLargeStraight(dice),0)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.LargeStraight),   0)
 
 
 # tests for rule 8: full house
+    def test_FullHouse(self):
+        dice = roll(1,1,1,2,2)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.FullHouse),   7)
+       
+    def test_NotFullHouse(self):
+        dice = roll(1,1,2,2,3)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.FullHouse),   0)
 
 # tests for rule 9: yahtzee (five of a kind)
+    def test_FiveOAK(self):
+        dice = roll(1,1,1,1,1)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Yahtzee),   5)
+       
+    def test_FiveOAKNoMatches(self):
+        dice = roll(1,1,2,2,3)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Yahtzee),   0)
 
 # tests for rule 10: chance
     def test_Chance(self):
         dice = roll(1,2,3,4,5)
-        self.assertEqual(Yahtzee.scoreChance(dice),15)
+        self.assertEqual(Yahtzee.score(dice,Yahtzee.YahtzeeScore.Chance),   15)
 
 def roll(d1, d2, d3, d4, d5):
     return [d1, d2, d3, d4, d5]
